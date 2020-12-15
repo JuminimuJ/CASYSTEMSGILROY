984b06b6055646e306f44b7782ca62248a4c2f1e
JuminimuJ <caballeroj84@gmail.com>
2020-12-15 15:25:31 +0800

    325

--- admin/modal/logout.php
+++ admin/modal/logout.php
@@ -2,7 +2,7 @@
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
-            <div class="modal-content">
+            <div class="modal-content rounded-0">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
@@ -11,8 +11,8 @@
                 </div>
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
+                    <button class="btn btn-secondary rounded-0" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-danger rounded-0" href="logout.php"><i class="fas fa-power-off"></i> Logout</a>
                 </div>
             </div>
         </div>
--- admin/modal/utils.php
+++ admin/modal/utils.php
@@ -74,3 +74,43 @@
     </div>
   </div>
 </div>
+
+
+<!-- MODAL UPD COLOR THEME -->
+<div class="modal fade" id="modalSettings" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content rounded-0">
+      <form method="post" action="update_logo.php">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-cogs"></i> Settings</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+            <?php 
+              $sql_color = "SELECT * FROM `acc_user` WHERE 1";
+              $query_color = $conn->query($sql_color);
+              $row_color = $query_color->fetch_array();
+            ?>
+            <i class="fas fa-cirle fa-3x" style="color:<?php echo $row_color[1];?>;"></i>
+        </div>
+
+        <div class="form-group">
+            <label>Change Admin Password:</label>
+            <input type="password" placeholder="Current Password" class="form-control rounded-0">
+            <label>New Password:</label>
+            <input type="password" placeholder="Your new Password" class="form-control rounded-0">
+            <input type="password" placeholder="Re-type Password" class="form-control rounded-0">
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary rounded-0" data-dismiss="modal">Close</button>
+        <button type="submit" name="btnupdcolor" class="btn btn-success rounded-0"><i class="fas fa-save"></i> Save Changes</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
